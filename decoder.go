8396e11b90af13a97d0b722ad215071bd25681c0
Kevin Golding <kevin@kgolding.co.uk>
2020-01-16 17:32:44 +0000

    add SeekByte

--- decoder.go
+++ decoder.go
@@ -5,13 +5,14 @@
 package decoder
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 )
 
 type Packet struct {
 	buf    []byte           // The raw data with dummy 0's appended
-	lenght int              // The actual data length
+	length int              // The actual data length
 	idx    int              // The current idx we've read up to
 	Err    error            // The last error
 	endian binary.ByteOrder // The endian to use for decoding
@@ -23,11 +24,30 @@ var ErrReadPastEndData = errors.New("read past of end of data")
 func New(b []byte) *Packet {
 	return &Packet{
 		buf:    b,
-		lenght: len(b),
+		length: len(b),
 		idx:    0,
 		Err:    nil,
 		endian: binary.BigEndian,
 	}
+}
+
+// Seek Byteto the next instance of the given byte
+func (p *Packet) SeekByte(b byte) bool {
+	return p.Seek([]byte{b})
+}
+
+// Seek to the next instance of the given bytes
+func (p *Packet) Seek(b []byte) bool {
+	if len(b) == 0 {
+		return false
+	}
+	for i := p.idx; i < len(p.buf)+len(b)-1; i++ {
+		if p.buf[i] == b[0] && bytes.Compare(p.buf[i:i+len(b)], b) == 0 {
+			p.idx = i + 1
+			return true
+		}
+	}
+	return false
 }
 
 // Index returns the current pointer position
@@ -64,7 +84,7 @@ func (p *Packet) Reset() {
 // Rewind moves the internal pointer backwards (or forward if passed a negative value)
 func (p *Packet) Rewind(i int) error {
 	idx := p.idx - i
-	if idx < 0 || idx >= p.lenght {
+	if idx < 0 || idx >= p.length {
 		return ErrReadPastEndData
 	}
 	p.idx = idx
